e221cfcfecb41adf0917c406db843bc45a2a2beb
ntlbbarbosa-gif <ntlbbarbosa@gmail.com>
2025-12-07 17:08:26 -0300

    Projeto LAVAMEPLUS - Site de limpeza de estofados

---
+++ src/pages/Index.tsx
@@ -0,0 +1,33 @@
+import { Header } from "@/components/Header";
+import { Hero } from "@/components/Hero";
+import { ProblemasSolucoes } from "@/components/ProblemasSolucoes";
+import { Precos } from "@/components/Precos";
+import { OPlus } from "@/components/OPlus";
+import { AntesDepois } from "@/components/AntesDepois";
+import { Depoimentos } from "@/components/Depoimentos";
+import { Beneficios } from "@/components/Beneficios";
+import { ChamadaFinal } from "@/components/ChamadaFinal";
+import { Footer } from "@/components/Footer";
+import { WhatsAppFloat } from "@/components/WhatsAppFloat";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Header />
+      <main>
+        <Hero />
+        <ProblemasSolucoes />
+        <Precos />
+        <OPlus />
+        <AntesDepois />
+        <Depoimentos />
+        <Beneficios />
+        <ChamadaFinal />
+      </main>
+      <Footer />
+      <WhatsAppFloat />
+    </div>
+  );
+};
+
+export default Index;
